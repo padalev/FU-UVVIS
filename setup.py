7f3b18d49baec22fb53e4a17a8faf811eec78e90
Ruben Nitsche <mail@rubennitsche.eu>
2023-01-10 12:24:14 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='FU-UVVIS',
+      version='0.1',
+      description='Spectroscopic utilities for Ocean Insight UVVIS spectrometers',
+      author='Ruben Nitsche',
+      author_email='mail@rubennitsche.eu',
+      url='https://github.com/padalev/FU-UVVIS',
+      packages=['UVVIS'],
+     )
